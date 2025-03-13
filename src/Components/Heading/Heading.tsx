81cf999256c9a1d3da931b1e427fa0a7a593cca0
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-03-13 08:54:19 +0600

    Add react framer motion.

--- src/Components/Heading/Heading.tsx
+++ src/Components/Heading/Heading.tsx
@@ -3,13 +3,30 @@ interface HeadingProps {
   subtitle: string;
 }
 
+import { motion } from "motion/react";
+import { fadeInAnimation } from "../../Animation/fadeInAnimation";
+
 const Heading = ({ title, subtitle }: HeadingProps) => {
   return (
     <div className="text-center space-y-2 w-fit mx-auto mb-2">
-      <h1 className="text-2xl md:text-3xl text-primary font-bold">{title}</h1>
-      <p className="text-secondary text-xs sm:text-sm max-w-sm mx-auto px-3">
+      <motion.h1
+        variants={fadeInAnimation("left", 0.2)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.2 }}
+        className="text-2xl md:text-3xl text-primary font-bold"
+      >
+        {title}
+      </motion.h1>
+      <motion.p
+        variants={fadeInAnimation("right", 0.3)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.3 }}
+        className="text-secondary text-xs sm:text-sm max-w-sm mx-auto px-3"
+      >
         {subtitle}
-      </p>
+      </motion.p>
     </div>
   );
 };
