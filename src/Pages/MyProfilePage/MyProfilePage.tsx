56a793641ae6c851cb0e771df3b9a4e11c734982
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-03-16 14:33:31 +0600

    Update my profile page.

--- src/Pages/MyProfilePage/MyProfilePage.tsx
+++ src/Pages/MyProfilePage/MyProfilePage.tsx
@@ -4,6 +4,7 @@ import useAuth from "../../Hooks/useAuth/useAuth";
 import defaultUser from "/defaultUser.svg";
 import useAxiosSecure from "../../Hooks/useAxios/useAxiosSecure";
 import LoadingSpinner from "../../Components/LoadingSpinner/LoadingSpinner";
+import { Link } from "react-router-dom";
 
 const MyProfilePage = () => {
   const { user } = useAuth();
@@ -25,30 +26,37 @@ const MyProfilePage = () => {
       <PageTitle title={"My Profile"} />
       <div className="primary-width mt-4 sm:mt-6 md:mt-8 lg:mt-10 mx-auto flex flex-col items-center justify-center">
         <div className="max-w-screen-sm w-full p-4 dark:bg-background2 rounded shadow-md">
-          <div className="text-center flex flex-col items-center justify-center">
-            <img
-              className="w-32 h-32 rounded-full"
-              src={user?.photoURL || defaultUser}
-              alt="profile"
-            />
-            <p className="text-xs text-secondary capitalize">
-              {myInfo.userType}
-            </p>
-            <h1 className="text-primary">{user?.displayName}</h1>
-            <p className="text-xs text-secondary">{user?.email}</p>
+          <div className="flex flex-col items-center justify-between sm:flex-row py-4 border-bottom">
+            <div className="flex flex-col sm:flex-row gap-2 sm:text-left items-center text-center">
+              <img
+                className="w-32 h-32 rounded-full"
+                src={user?.photoURL || defaultUser}
+                alt="profile"
+              />
+              <div>
+                <p className="text-xs text-secondary capitalize">
+                  {myInfo.userType}
+                </p>
+                <h1 className="text-primary">{user?.displayName}</h1>
+                <p className="text-xs text-secondary">{user?.email}</p>
+              </div>
+            </div>
+            <Link to={"/my-events"} className="mt-2 sm:mt-0">
+              <button className="primary-btn outline-btn">My Events</button>
+            </Link>
           </div>
           <div className="overflow-x-auto w-full mt-4">
-            <h1 className="text-center text-primary">Post History</h1>
-            <table className="table text-secondary text-center border-none">
+            <h1 className="text-center text-primary">Event History</h1>
+            <table className="table text-secondary text-center">
               <thead className="text-secondary">
-                <tr className="border-none">
+                <tr className="">
                   <th>Total</th>
                   <th>Approved</th>
                   <th>Pending</th>
                 </tr>
               </thead>
-              <tbody className="border-none">
-                <tr className="border-none">
+              <tbody>
+                <tr>
                   <td>{myInfo.totalPosts}</td>
                   <td>{myInfo.approved}</td>
                   <td>{myInfo.totalPosts - myInfo.approved}</td>
