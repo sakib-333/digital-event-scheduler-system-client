e399eb7425b397eabd9e14c7c5c17204307f7aa4
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-03-16 11:11:03 +0600

    Add approve option in manage event page.

--- src/Pages/ManageEventPage/ManageEventPage.tsx
+++ src/Pages/ManageEventPage/ManageEventPage.tsx
@@ -1,15 +1,20 @@
 import { MdDeleteForever } from "react-icons/md";
 import EventDetails from "../../Components/EventDetails/EventDetails";
 import PageTitle from "../../Components/PageTitle/PageTitle";
+import { FaRegCheckCircle } from "react-icons/fa";
 
 const ManageEventPage = () => {
   return (
     <div className="max-w-screen-md mx-auto dark:bg-background2">
       <PageTitle title="Manage Event" />
       <EventDetails />
-      <div className="flex justify-center p-4">
-        <button className="primary-btn flex items-center gap-1">
-          <MdDeleteForever className="text-[#f00]" />
+      <div className="flex justify-center gap-2 p-4">
+        <button className="primary-btn text-black bg-green-300 flex items-center gap-1">
+          <FaRegCheckCircle />
+          <span>Approve</span>
+        </button>
+        <button className="primary-btn text-black bg-red-300 flex items-center gap-1">
+          <MdDeleteForever />
           <span>Delete</span>
         </button>
       </div>
