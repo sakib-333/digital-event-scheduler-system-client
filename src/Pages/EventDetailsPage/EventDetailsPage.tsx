9ab0bb1def940ec2c632064a507a3f141ec94b50
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-04-06 21:33:34 +0600

    Get email alert for an event.

--- src/Pages/EventDetailsPage/EventDetailsPage.tsx
+++ src/Pages/EventDetailsPage/EventDetailsPage.tsx
@@ -7,8 +7,14 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../Hooks/useAxios/useAxiosPublic";
 import LoadingSpinner from "../../Components/LoadingSpinner/LoadingSpinner";
 import NoDataFound from "../../Components/NoDataFound/NoDataFound";
+import { successAlert } from "../../Components/Alerts/successAlert";
+import { errorAlert } from "../../Components/Alerts/errorAlert";
+import emailjs from "@emailjs/browser";
+import { formatDate } from "../../Utils/formatDate";
+import { useState } from "react";
 
 const EventDetailsPage = () => {
+  const [loading, setLoading] = useState<Boolean>(false);
   const { user } = useAuth();
   const axiosPublic = useAxiosPublic();
   const navigate = useNavigate();
@@ -30,6 +36,37 @@ const EventDetailsPage = () => {
       return navigate("/login");
     }
     // Send alert to currently logged in user email.
+    const today = new Date();
+    const eventDate = new Date(event?.date);
+
+    if (today < eventDate) {
+      const templateParams = {
+        senderName: "Sakibur Rahman",
+        receiverName: user?.displayName,
+        message: `${event?.title} is coimg on ${formatDate(event?.date)} in ${
+          event?.location
+        }. Please join in time.`,
+        receiverEmail: user?.email,
+      };
+
+      setLoading(true);
+      emailjs
+        .send(
+          import.meta.env.VITE_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          templateParams,
+          import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        )
+        .then(() => {
+          successAlert("Success", "We have send an alert.");
+        })
+        .catch(() => {
+          errorAlert("Failed", "Sorry can not send email");
+        })
+        .finally(() => setLoading(false));
+    } else {
+      errorAlert("Failed", "Sorry this event has finished");
+    }
   };
 
   if (isLoading) {
@@ -45,13 +82,22 @@ const EventDetailsPage = () => {
       <PageTitle title="Details" />
       <EventDetails event={event} />
       <div className="flex justify-center p-4">
-        <button
-          onClick={handlegetAlert}
-          className="primary-btn flex items-center gap-1"
-        >
-          <MdAddAlert />
-          <span>Get Alert</span>
-        </button>
+        {loading ? (
+          <button
+            disabled
+            className="primary-btn cursor-not-allowed flex items-center gap-1"
+          >
+            <span>Loading...</span>
+          </button>
+        ) : (
+          <button
+            onClick={handlegetAlert}
+            className="primary-btn flex items-center gap-1"
+          >
+            <MdAddAlert />
+            <span>Get Alert</span>
+          </button>
+        )}
       </div>
     </div>
   );
