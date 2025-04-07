8d83173b68dc3dfcaa37d48832de660606d20518
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-04-07 11:06:06 +0600

    Fetch up coming events.

--- src/Pages/HomePage/UpComingEvents/UpComingEvents.tsx
+++ src/Pages/HomePage/UpComingEvents/UpComingEvents.tsx
@@ -2,40 +2,47 @@ import { Link } from "react-router-dom";
 import EventCard from "../../../Components/EventCard/EventCard";
 import Heading from "../../../Components/Heading/Heading";
 import { EventCardInterface } from "../../EventsPage/EventsPage";
-
-const event: EventCardInterface = {
-  _id: "12g22",
-  title: "2.2 Semester Exam",
-  category: "Exam",
-  date: "2025-04-01T00:00:00.000+00:00",
-  description:
-    "The Semester Exam is a final assessment that evaluates students' understanding of the material covered throughout the course. It tests both theoretical knowledge and practical skills, aiming to measure overall academic progress. The exam includes various question types, such as multiple-choice, essays, and problem-solving tasks. It serves as an opportunity for students to demonstrate their mastery of key concepts.",
-  location: "Academic Building",
-  photo: "https://i.ibb.co/Y47M0p30/exam.jpg",
-};
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../../Hooks/useAxios/useAxiosPublic";
+import LoadingSpinner from "../../../Components/LoadingSpinner/LoadingSpinner";
+import NoDataFound from "../../../Components/NoDataFound/NoDataFound";
 
 const UpComingEvents = () => {
+  const axiosPublic = useAxiosPublic();
+  const { data: upComingEvents, isLoading } = useQuery({
+    queryKey: ["upComingEvents"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/up-coming-events");
+      if (res.data.acknowledged) {
+        return res.data.upComingEvents;
+      }
+      return res.data.message;
+    },
+  });
   return (
     <div className="primary-spacing primary-width mx-auto">
       <Heading
         title="Up Coming Event"
         subtitle="Stay with us for the coming events."
       />
-      <div className="w-full">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4">
-          <EventCard key={Math.random()} event={event} />
-          <EventCard key={Math.random()} event={event} />
-          <EventCard key={Math.random()} event={event} />
-          <EventCard key={Math.random()} event={event} />
-          <EventCard key={Math.random()} event={event} />
-          <EventCard key={Math.random()} event={event} />
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : upComingEvents === "No data found" ? (
+        <NoDataFound />
+      ) : (
+        <div className="w-full">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4">
+            {upComingEvents.map((event: EventCardInterface) => (
+              <EventCard key={event._id} event={event} />
+            ))}
+          </div>
+          <div className="w-full flex justify-center mt-8 sm:mt-10 md:mt-12 lg:mt-16">
+            <Link to={"/events"}>
+              <button className="primary-btn outline-btn">See More</button>
+            </Link>
+          </div>
         </div>
-        <div className="w-full flex justify-center mt-8 sm:mt-10 md:mt-12 lg:mt-16">
-          <Link to={"/events"}>
-            <button className="primary-btn outline-btn">See More</button>
-          </Link>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
