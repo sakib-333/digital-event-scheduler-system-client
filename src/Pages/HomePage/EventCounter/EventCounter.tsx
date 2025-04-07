07ab8e05fd2f4e5783a9d8de5129bd45741e9b86
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-04-07 11:38:28 +0600

    Update event counter

--- src/Pages/HomePage/EventCounter/EventCounter.tsx
+++ src/Pages/HomePage/EventCounter/EventCounter.tsx
@@ -1,14 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
 import Counter from "../../../Components/Counter/Counter";
 import Heading from "../../../Components/Heading/Heading";
+import useAxiosPublic from "../../../Hooks/useAxios/useAxiosPublic";
 
 const EventCounter = () => {
+  const axiosPublic = useAxiosPublic();
+  const { data: eventCount } = useQuery({
+    queryKey: ["eventCounter"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/count-events");
+      if (res.data.acknowledged) {
+        return {
+          totalEvents: res.data.totalEvents,
+          completedEvents: res.data.completedEvents,
+        };
+      }
+      return {
+        totalEvents: 0,
+        completedEvents: 0,
+      };
+    },
+  });
   return (
     <div className="primary-spacing bg-background2 primary-width mx-auto">
       <Heading title="Event Counter" subtitle="All of our events" />
       <div className="grid grid-cols-1 sm:grid-cols-3 justify-items-center gap-4">
-        <Counter eventType={"Total events"} range={100} />
-        <Counter eventType={"Completed events"} range={60} />
-        <Counter eventType={"Upcoming events"} range={40} />
+        <Counter
+          eventType={"Total events"}
+          range={eventCount?.totalEvents || 0}
+        />
+        <Counter
+          eventType={"Completed events"}
+          range={eventCount?.completedEvents || 0}
+        />
+        <Counter
+          eventType={"Upcoming events"}
+          range={eventCount?.totalEvents - eventCount?.completedEvents || 0}
+        />
       </div>
     </div>
   );
